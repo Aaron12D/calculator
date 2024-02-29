c5123df704db17266973d116f39542735eb526e0
Aaron12D <153545330+Aaron12D@users.noreply.github.com>
2024-02-28 19:33:33 -0500

    add javascript function to the number buttons

--- script.js
+++ script.js
@@ -1,55 +1,95 @@
 const currentAnswer = document.querySelector('#current-answer');
+const oldAnswer = document.querySelector('#old-answer');
+let oldAnswerTotal = '-';
+let currentNum = [];
+let secondNum = null;
+let operand = null;
 let total = null;
-let totalStr = ['1', '+' , '2'];
+let totalStr = [];
 
-function checkOperand(event){
-    if (currentAnswer.textContent){
 
-    }
+function checkOperand(){
+    const lastNum = parseInt(totalStr.slice(-1));
+    if (isNaN(lastNum)){
+    } else {
+        totalStr.push(operand);
+        console.log(totalStr.join(''));
+        
+        currentNum = [];
+        // const reversedStr = totalStr.slice().reverse().join('')
+        // currentAnswer.textContent = `${reversedStr}`;
+        console.log(totalStr);
+    };
 }
+
 function getTotal(){
     total = totalStr.join('')
-    currentAnswer.textContent = total
+    return new Function('return ' + total)();
 };
-getTotal();
 
 document.addEventListener('click', function(event){
-    console.log(event.target.id)
-    if (event.target.id === 'equal'){
-        currentAnswer.textContent = total
+    console.log(event.target.className)
+    if (event.target.id === 'equal' && totalStr.length >= 3){
+        currentAnswer.textContent = getTotal();
+        oldAnswerTotal = `${getTotal()} = ${totalStr.join('')}`;
+        totalStr = [];
+        currentNum = [];
     } else if (event.target.id === 'clear'){
-        total
+        totalStr = [];
+        currentNum = [];
+        currentAnswer.textContent = 0;
+        oldAnswer.textContent = oldAnswerTotal;
     } else if (event.target.id === 'plus'){
-
+        operand = '+';
+        checkOperand();
     } else if (event.target.id === 'minus'){
-        
+        operand = '-';
+        checkOperand();
     } else if (event.target.id === 'multiply'){
-
+        operand = '*'
+        checkOperand();
     } else if (event.target.id === 'divide'){
-
+        operand = '/'
+        checkOperand();
     } else if (event.target.id === 'percent'){
-
+        checkOperand();
     } else if (event.target.id === 'negative'){
-
-    } else if (event.target.id === 'seven'){
+        if (parseInt(currentNum.join('')) > 0){
+            totalStr.splice(-currentNum.length, currentNum.length, `-${currentNum}`);
+            currentNum.splice(-currentNum.length, currentNum.length, `-${currentNum}`)
+            const reverseNum = currentNum.splice().reverse().join('');
+            currentAnswer.textContent = reverseNum;
+        };
+        console.log(currentNum)
         
-    } else if (event.target.id === 'eight'){
+    } else if (event.target.id === 'decimal'){
 
-    } else if (event.target.id === 'nine'){
-
-    } else if (event.target.id === 'four'){
-
-    } else if (event.target.id === 'five'){
-
-    } else if (event.target.id === 'six'){
-
-    } else if (event.target.id === 'one'){
-
-    } else if (event.target.id === 'two'){
-
-    } else if (event.target.id === 'three'){
-
-    } else if (event.target.id === 'zero'){
-
+    } else if (event.target.className === 'main'){      
+        currentNum.push(event.target.textContent)
+        totalStr.push(event.target.textContent);
+        currentAnswer.textContent = currentNum.join('');
     };
+    
+
+    // } else if (event.target.id === 'seven'){
+        
+    // } else if (event.target.id === 'eight'){
+
+    // } else if (event.target.id === 'nine'){
+
+    // } else if (event.target.id === 'four'){
+
+    // } else if (event.target.id === 'five'){
+
+    // } else if (event.target.id === 'six'){
+
+    // } else if (event.target.id === 'one'){
+
+    // } else if (event.target.id === 'two'){
+
+    // } else if (event.target.id === 'three'){
+
+    // } else if (event.target.id === 'zero'){
+
+    // };
 });
